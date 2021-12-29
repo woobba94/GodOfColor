5ca246f9cb3d4c85adb5d77a15417092efdffc43
woobba94 <jwj3199@naver.com>
2021-12-29 21:49:39 +0900

    ✅ Update: restart

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const cover = document.querySelector('.cover');
+const startBtn = document.querySelector('.start');
+startBtn.addEventListener('click', init);
+
 const display = document.querySelector('.display');
 const heartList = document.querySelector('.heart-wrap');
 const stageBox = document.querySelector('.stage-box');
@@ -19,6 +23,16 @@ let targetIndex = {
   x: Math.floor(Math.random() * (level + 1)),
   y: Math.floor(Math.random() * (level + 1)),
 };
+
+function start() {
+  heartList.innerHTML = `<i class="fas fa-heart"></i>
+  <i class="fas fa-heart"></i>
+  <i class="fas fa-heart"></i>
+  <i class="fas fa-heart"></i>
+  <i class="fas fa-heart"></i>`;
+  stageBox.textContent = `stage : ${stage}`;
+  next();
+}
 
 function setDisplay() {
   for (let i = 0; i < level + 1; i++) {
@@ -44,13 +58,18 @@ function setDisplay() {
   }
 }
 
-function init() {
+function next() {
   resetTimer();
   startTimer();
   clearDisplay();
   setBaseColor();
   setTarget();
   setDisplay();
+}
+
+function init() {
+  startBtn.remove();
+  start();
 }
 
 function clearDisplay() {
@@ -89,7 +108,7 @@ function success() {
     level++;
     successCount = 0;
   }
-  init();
+  next();
 }
 
 function fail() {
@@ -99,7 +118,6 @@ function fail() {
   if (failCount === 5) {
     clearInterval(interval);
     end();
-
     result();
   }
 }
@@ -130,20 +148,41 @@ function updateTimer() {
 function end() {
   const target = document.querySelector('.target');
   target.style.boxShadow = `inset 0px 0px ${20 / level}px #000000`;
-  display.outerHTML = display.outerHTML;
+  cover.style.zIndex = '10';
 }
 
 function result() {
   let result = '';
-  if (stage > 100) result = '당신은 혹시.. 신입니까?';
-  else if (stage > 80) result = '당신의 감각은 인류의 자랑입니다.';
-  else if (stage > 70) result = '당신의 어쩌면.. 맹금류입니다.';
-  else if (stage > 60) result = '당신의 야생동물적인 감각을 가지고 있습니다.';
-  else if (stage > 45) result = '당신의 컬러감각은 미대생 수준입니다.';
-  else if (stage > 30) result = '당신의 컬러감각은 일반적인 수준입니다.';
-  else if (stage > 10) result = '당신은 초보수준 컬러감각을 가지고 있습니다.';
+  if (stage >= 100) result = '당신은 혹시.. 신입니까?';
+  else if (stage >= 80) result = '당신의 감각은 인류의 자랑입니다.';
+  else if (stage >= 70) result = '당신의 어쩌면.. 맹금류입니다.';
+  else if (stage >= 60) result = '당신의 야생동물적인 감각을 가지고 있습니다.';
+  else if (stage >= 45) result = '당신의 컬러감각은 미대생 수준입니다.';
+  else if (stage >= 30) result = '당신의 컬러감각은 일반적인 수준입니다.';
+  else if (stage >= 10) result = '당신은 초보수준 컬러감각을 가지고 있습니다.';
   else result = '음.. 조금만 제대로 플레이 해보시겠습니까?';
 
-  resultBox.innerHTML = `최종 스테이지 : ${stage - 1}<br>
-  <p class="result-text">${result}</p>`;
+  resultBox.innerHTML = `최종 스테이지 : ${stage}<br>
+  <p class="result-text">${result}</p>
+  <button class="restart">다시 도전</button>`;
+  const restartBtn = document.querySelector('.restart');
+  restartBtn.addEventListener('click', restart);
+}
+
+function restart() {
+  TIME = MAX_TIME;
+  stage = 1;
+  level = 1;
+  successCount = 0;
+  failCount = 0;
+  colorPoint = 0;
+  baseColor = '';
+  targetColor = '';
+  targetIndex = {
+    x: Math.floor(Math.random() * (level + 1)),
+    y: Math.floor(Math.random() * (level + 1)),
+  };
+  cover.style.zIndex = '-10';
+  resultBox.innerHTML = '';
+  start();
 }
